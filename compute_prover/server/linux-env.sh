d5fe2912fada82fe49af40cbd79c87c4614a5c57
Bill Gleim <bgleim@gmail.com>
2018-10-13 16:13:56 -0700

    Create linux-env.sh

---
+++ compute_prover/server/linux-env.sh
@@ -0,0 +1,18 @@
+sudo apt update
+sudo apt upgrade
+sudo apt dist-upgrade
+sudo apt install python-minimal
+sudo apt install libz-dev
+# Ubuntu 16.04 libsnark dependencies
+sudo apt-get install build-essential cmake git libgmp3-dev libprocps4-dev python-markdown libboost-all-dev libssl-dev
+sudo apt install pkg-config
+# node package manager
+sudo apt install npm
+# node.js
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt install nodejs
+# java
+sudo apt install default-jre
+sudo apt install default-jdk
+sudo apt install ant
+sudo apt install golang-go
