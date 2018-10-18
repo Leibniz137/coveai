ab6464d7245f20876eb1da3454a85615c4fd6f94
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2018-10-18 17:18:11 +0100

    add: run script specifying dependency location on the fly

---
+++ compute_prover/server/run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+trap on_exit INT
+
+# clean submodule
+function on_exit() {
+  cd pequin/pepper && \
+  git reset --hard && \
+  git clean -x -f -d
+  exit $?
+}
+
+# enable precedence of environment variables
+sed -i 's,DEPSDIR := ,DEPSDIR ?= ,' pequin/pepper/Makefile
+# copy new app
+cp icf_cy_pam_clustering.sfdl pequin/pepper/apps
+
+DEPSDIR="$(pwd)/pepper_deps" node coveai_server.js
+
