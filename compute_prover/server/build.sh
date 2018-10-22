f90d397f071852c8dde1e967af03a08cc1d6885e
Jonathan Passerat-Palmbach <j.passerat-palmbach@imperial.ac.uk>
2018-10-23 00:43:12 +0100

    enh: add node package install to build script and simplify README (fix #5)

--- compute_prover/server/build.sh
+++ compute_prover/server/build.sh
@@ -3,6 +3,9 @@
 # missing libraries (Debian testing)
 #sudo apt install libevent-dev libopenmpi-dev libhwloc-dev libboost-program-options-dev
 
+# install node dependencies
+npm install express cors
+
 # install in current directory
 # overriding flags that wouldn't allow libfmt to compile (-Werror)
 (HOME_HACK=$(pwd) && \
